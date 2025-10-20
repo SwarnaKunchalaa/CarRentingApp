574c628cdc821021da84d9b29f312b40263bdd21
SwarnaKunchalaa <swarnakunchala1317@gmail.com>
2025-10-20 22:19:29 +0530

    added get Booking functionality

--- src/app/pages/booking/booking.ts
+++ src/app/pages/booking/booking.ts
@@ -1,11 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { BookingModel } from '../../model/booking';
+import { HttpClient } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-booking',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './booking.html',
   styleUrl: './booking.css'
 })
 export class Booking {
 
+  newBookingObj: BookingModel;
+
+  http = inject(HttpClient);
+
+  constructor() {
+    this.newBookingObj = new BookingModel();
+  }
+
+  onSaveBooking() {
+    this.http.post(`https://freeapi.miniprojectideas.com/api/CarRentalApp/CreateNewBooking`, this.newBookingObj)
+      .subscribe((res: any) => {
+        if (res.result) {
+          alert("Booking saved successfully");
+        } else {
+          alert("Failed to save booking: " + res.message);
+        }
+      });
+
+  }
 }
